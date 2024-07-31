5f8bf7f3b96b098469b1983189a23d79943c59f7
michael <michael@neustater.com>
2024-07-31 11:47:08 -0400

    Added winning to each side

--- src/App.jsx
+++ src/App.jsx
@@ -36,24 +36,25 @@ function Board({xIsNext, setXIsNext}) {
     setSquares(nextSquares);
     setXIsNext(!xIsNext);
   }
+  let hasWon = checkWinner(squares);
+  let hasWinner = hasWon === "X" ? (<div className="result x-result"><p>X</p></div>) : hasWon === "O" ? (<div className="result o-result"><p>O</p></div>) : null;
 
   return (
     <>
-      <div className="board-row">
+      <div className="board">
         <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
         <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
         <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
-      </div>
-      <div className="board-row">
+
         <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
         <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
         <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
-      </div>
-      <div className="board-row">
+
         <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
         <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
         <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
       </div>
+      {hasWinner}
     </>
   );
 }
